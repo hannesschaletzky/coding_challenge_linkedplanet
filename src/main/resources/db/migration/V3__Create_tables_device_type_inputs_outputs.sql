27a1dd48665fe940b47c8acec921a790ce6ffaaf
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2024-05-30 01:14:31 +0200

    rework migrations

--- src/main/resources/db/migration/V3__Create_tables_device_type_inputs_outputs.sql
+++ src/main/resources/db/migration/V3__Create_tables_device_type_inputs_outputs.sql
@@ -1,15 +1,15 @@
 CREATE TABLE device_type_inputs (
+    id SERIAL PRIMARY KEY,
     device_type_name VARCHAR(100) NOT NULL,
     input_device_type_name VARCHAR(100) NOT NULL,
-    PRIMARY KEY (device_type_name, input_device_type_name),
     FOREIGN KEY (device_type_name) REFERENCES device_types(name),
     FOREIGN KEY (input_device_type_name) REFERENCES device_types(name)
 );
 
 CREATE TABLE device_type_outputs (
+    id SERIAL PRIMARY KEY,
     device_type_name VARCHAR(100) NOT NULL,
     output_device_type_name VARCHAR(100) NOT NULL,
-    PRIMARY KEY (device_type_name, output_device_type_name),
     FOREIGN KEY (device_type_name) REFERENCES device_types(name),
     FOREIGN KEY (output_device_type_name) REFERENCES device_types(name)
 );
